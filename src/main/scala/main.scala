0e630a1b556d68c4e0164c20a29819b65ff12c16
maxime <maxime@mail.fr>
2022-12-28 20:06:21 +0100

    first

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,19 @@
 package fr.maxime.binandco
 
+import java.nio.charset.Charset
+
 @main
 def main(): Unit = {
-  println("Hello world!")
+  val text = "Hello world!"
+  println(text)
+  printBinariesInfoAboutText(text)
+}
+
+def printBinariesInfoAboutText(text:String): Unit ={
+
+  val bytes = text.getBytes(Charset.forName("UTF-8"))
+  for(index <- bytes.indices){
+    println(s"'${text.apply(index)}'= 0x${bytes.apply(index).toInt.toHexString} = 0b${bytes.apply(index).toInt.toBinaryString}")
+  }
+
 }
