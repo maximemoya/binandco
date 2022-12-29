4462055e4605454af8499e84911efa6d0d5f35c3
Maxime <maxime@mail.fr>
2022-12-29 11:00:20 +0100

    organizeTextWithLengthEndLine WorkInProgress

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,7 @@
 package fr.maxime.binandco
 
 import java.nio.charset.Charset
+import scala.util.control.Breaks.{break, breakable}
 
 @main
 def main(): Unit = {
@@ -10,8 +11,15 @@ def main(): Unit = {
   val text127 = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
   val text128 = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
   val text129 = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
-  val text = text129
-  organizeTextIn64BytesBlocks(text)
+  val text239 = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+  val text240 = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+  val text241 = "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+  val text = text64
+
+  //  organizeTextIn64BytesBlocks(text)
+  organizeTextIn64BytesBlocksWithLengthEndLine(text)
+
+  //  organizeTextIn256BytesBlocksWithLengthEndLine(text)
 }
 
 def printBinariesInfoAboutText(text: String): Unit = {
@@ -26,7 +34,6 @@ def printBinariesInfoAboutText(text: String): Unit = {
 def organizeTextIn64BytesBlocks(text: String): Unit = {
 
   val blockSize = 64
-
   val bytes = text.getBytes(Charset.forName("UTF-8"))
   val blocksLength = ((bytes.length - 1) / blockSize) + 1
   val blocksRest = (blocksLength * blockSize) - bytes.length
@@ -58,3 +65,117 @@ def organizeTextIn64BytesBlocks(text: String): Unit = {
 
 }
 
+def organizeTextIn64BytesBlocksWithLengthEndLine(text: String): Unit = {
+
+  val blockSize = 64
+  val endLineLength = 8
+  val textBytes = text.getBytes(Charset.forName("UTF-8"))
+
+  // ok for textBytes.length from 0 to 256 but need to continue for > 256
+  val textBytesHexLengthByte = textBytes.length.toByte
+  val textBytesHexLengthBytes = new Array[Byte](1)
+  textBytesHexLengthBytes.update(0, textBytesHexLengthByte)
+
+  val bytesLength = textBytes.length + endLineLength
+  val bytes: Array[Byte] = new Array(bytesLength)
+  breakable {
+    for (i <- bytes.indices) {
+      if (i < textBytes.length) bytes.update(i, textBytes.apply(i))
+      else break
+    }
+  }
+  for (i <- textBytesHexLengthBytes.indices) {
+    bytes.update(bytesLength - 1 - i, textBytesHexLengthBytes.apply(textBytesHexLengthBytes.length - 1 - i))
+  }
+
+  printN1ByteArray(textBytesHexLengthBytes)
+  printN1ByteArray(bytes)
+
+  val blocksLength = ((bytes.length - 1) / blockSize) + 1
+  val blocksRest = (blocksLength * blockSize) - bytes.length
+
+  println(s"text: $text")
+  println(s"textBytes            : ${textBytes.length} bytes")
+  println(s"textBytes hex-length : ${textBytesHexLengthBytes.length} bytes")
+  println(s"bytes length         : ${textBytes.length} + $endLineLength = ${bytes.length} bytes")
+  println(s"blocks size          : $blocksLength * $blockSize = ${blocksLength * blockSize} bytes")
+  println(s"blocks rest with 0   : ${blocksLength * blockSize} - ${bytes.length} = $blocksRest bytes")
+
+  val blocks: Array[Array[Byte]] = new Array(blocksLength)
+
+  for (i <- 0 until blocksLength) {
+    val block: Array[Byte] = new Array(blockSize)
+    for (j <- block.indices) {
+      if (((i * blockSize) + j) < bytes.length) {
+        block.update(j, bytes.apply(j))
+      }
+      else {
+        block.update(j, 0x00)
+      }
+    }
+    blocks.update(i, block)
+  }
+
+  printN2ByteArray(blocks)
+  //  println("\nBlocks info :\n[")
+  //  for (i <- blocks) {
+  //    println(s"[ ${i.mkString("-")} ],")
+  //  }
+  //  println("]")
+
+}
+
+def organizeTextIn256BytesBlocksWithLengthEndLine(text: String): Unit = {
+
+  val blockSize = 256
+  val endLineLength = 16
+  val textBytes = text.getBytes(Charset.forName("UTF-8"))
+  val textBytesHexLengthBytes = textBytes.length.toHexString.getBytes(Charset.forName("UTF-8"))
+
+  val bytesLength = textBytes.length + endLineLength
+  val bytes: Array[Byte] = new Array(bytesLength)
+  for (i <- bytes.indices) {
+    if (i < textBytes.length) bytes.update(i, textBytes.apply(i))
+    else bytes.update(i, 0x00)
+  }
+
+  println(s"[ ${bytes.mkString("-")} ]")
+
+  val blocksLength = ((bytes.length - 1) / blockSize) + 1
+  val blocksRest = (blocksLength * blockSize) - bytes.length
+
+  println(s"text: $text")
+  println(s"textBytes            : ${textBytes.length} bytes")
+  println(s"textBytes hex-length : ${textBytesHexLengthBytes.length} bytes")
+  println(s"bytes length         : ${textBytes.length} + $endLineLength = ${bytes.length} bytes")
+  println(s"blocks size          : $blocksLength * $blockSize = ${blocksLength * blockSize} bytes")
+  println(s"blocks rest with 0   : ${blocksLength * blockSize} - ${bytes.length} = $blocksRest bytes")
+
+  // to continue =>
+
+  val blocks: Array[Array[Byte]] = new Array(blocksLength)
+
+  for (i <- 0 until blocksLength) {
+    val block: Array[Byte] = new Array(blockSize)
+    for (j <- block.indices) {
+      if (((i * blockSize) + j) < bytes.length) block.update(j, bytes.apply(j))
+      else block.update(j, 0x00)
+    }
+    blocks.update(i, block)
+  }
+
+  //  printSqrtByteArray(blocks)
+
+}
+
+def printN1ByteArray(array: Array[Byte]): Unit = {
+  println(s"[ ${array.map { byte => String.format("%02x", byte) }.mkString("-")} ]")
+}
+
+def printN2ByteArray(sqrtArray: Array[Array[Byte]]): Unit = {
+  println("\nSquareArray info :\n[")
+  for (byte <- sqrtArray) {
+    println(s"[ ${byte.map { element => String.format("%02x", element) }.mkString("-")} ],")
+  }
+  println("]")
+}
