60b7a68dab7d543d06e306465c393ed2e9d13e92
Maxime <maxime@mail.fr>
2023-01-29 21:50:43 +0100

    randInt

--- src/main/scala/tools/swing/MainSwing.scala
+++ src/main/scala/tools/swing/MainSwing.scala
@@ -1,9 +1,14 @@
 package fr.maxime.binandco
 package tools.swing
 
-import java.awt.FlowLayout
+import java.awt.*
 import java.awt.event.ActionListener
-import javax.swing.*
+import java.io.{File, IOException, PrintWriter}
+import java.time.Instant
+import java.util.{Date, UUID}
+import javax.imageio.ImageIO
+import javax.swing.{ImageIcon, JButton, JFrame, JLabel, JPanel, JTextArea, Timer, WindowConstants}
+import scala.util.Random
 
 object Timer {
   def apply(interval: Int, repeats: Boolean = true)(op: => Unit): Unit = {
@@ -21,17 +26,23 @@ class MainSwing {
   val frame = new JFrame("JFrame Example")
   val panel = new JPanel()
   panel.setLayout(new FlowLayout())
-  val label = new JLabel("000000000.0")
-  val button = new JButton()
-  Timer(1000){label.setText((Math.random()*100_000_000).toInt.toString)}
+  val label = new JLabel("0")
+  label.setFont(new Font("",0,20))
+  val rand = new Random()
+  Timer(1000) {
+    label.setText(String.format("%12s",rand.nextInt().abs).replace(" ", "0"))
+  }
 
-  button.setText("Button")
+  //  val button = new JButton()
+//  button.setText("Button")
+
   panel.add(label)
-  panel.add(button)
+//  panel.add(button)
   frame.add(panel)
-  frame.setSize(200, 300)
+
+  frame.setSize(300, 200)
   frame.setLocationRelativeTo(null)
-  frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE)
+  frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
   frame.setVisible(true)
 
 }
